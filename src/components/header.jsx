33cae1d9ef828f3a71fa8e052ec742415471569a
Aman-Sidd <amansiddiqui9804509986@gmail.com>
2024-06-08 01:52:09 +0530

    removing boilerplate texts

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,11 +8,27 @@ export const Header = (props) => {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
+                <h1 className="gradient-text">
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <div>
+                  <a
+                    href="https://print-ease.netlify.app"
+                    className="btn btn-custom btn-lg page-scroll"
+                    style={{ margin: 10 }}
+                  >
+                    Web App{" "}
+                  </a>
+                  <a
+                    href="#pageToTop"
+                    className="btn btn-custom btn-lg page-scroll"
+                    style={{ margin: 10 }}
+                  >
+                    Get APK{" "}
+                  </a>
+                </div>
                 <a
                   href="#features"
                   className="btn btn-custom btn-lg page-scroll"
